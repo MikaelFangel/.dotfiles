8e97987ca770b1f0dd1f446c3399cd472792bb08
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2023-06-20 09:09:18 +0200

    updated power management

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -61,13 +61,12 @@
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "powersave";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
-	# Frequency in KHz
-        CPU_SCALING_MIN_FREQ_ON_AC = 800000;
-        CPU_SCALING_MAX_FREQ_ON_AC = 5000000;
-        CPU_SCALING_MIN_FREQ_ON_BAT = 800000;
-        CPU_SCALING_MAX_FREQ_ON_BAT = 2600000;
+	CPU_MIN_PERF_ON_AC = 0;
+	CPU_MAX_PERF_ON_AC = 100;
+	CPU_MIN_PERF_ON_BAT = 0;
+	CPU_MAX_PERF_ON_BAT = 20;
       };
     };
   
