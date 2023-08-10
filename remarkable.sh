8fd2d42b029c056c30e003a2914c047acb94d4bd
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2023-08-10 13:30:57 +0200

    removed old config files

--- remarkable.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/env bash
-
-dev=enp0s20f0u3c2
-ip link set $dev up
-ip route add 10.11.99.1 dev $dev 
--- scrot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-
-path="$HOME/Pictures"
-if ! [ -d "$path" ]; then
-  mkdir -p "$path"
-fi
-
-grim -g "$(slurp -w 2)" "$path/screen-$(date '+%Y-%m-%d-%H:%M:%S').jpg"
